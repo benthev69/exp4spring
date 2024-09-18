5ccfeaf740a35aa53a325b7848677dd3b7074421
benthev69 <149101870+benthev69@users.noreply.github.com>
2024-09-18 15:13:00 +0200

    Update Dockerex1ApplicationTests.java

--- src/test/java/sesame/com/Dockerex1ApplicationTests.java
+++ src/test/java/sesame/com/Dockerex1ApplicationTests.java
@@ -1,13 +1,1 @@
-package sesame.com;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Dockerex1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
